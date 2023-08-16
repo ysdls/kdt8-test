f9280ad2440b87c03b3b20e0e4f724022442aecc
ysdls <ysdls84@gmail.com>
2023-08-16 09:23:18 +0900

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//router 분리
+app.get('/', (req, res) => {
+    res.render('index');
+});
+const router = require('./routes');
+app.use('/visitor', router);
+
+//오류처리
+app.use('*', (req, res) => {
+    res.render('404');
+});
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+});
